6e4ae3156312a1610b2cb9e274031c3e6a43e3b0
Rafael Siqueira <rvsiqueira@users.noreply.github.com>
2018-05-07 09:55:57 -0300

    Implemented Lost Password

--- lambdaUser.js
+++ lambdaUser.js
@@ -62,6 +62,85 @@ function sendVerificationEmail(email, token, fn) {
 	}, fn);
 }
 
+function getUserLostPassword(email, fn) {
+	dynamodb.getItem({
+		TableName: config.DDB_USER_TABLE,
+		Key: {
+			email: {
+				S: email
+			}
+		}
+	}, function(err, data) {
+		if (err) return fn(err);
+		else {
+			if ('Item' in data) {
+				fn(null, email);
+			} else {
+				fn(null, null); // User not found
+			}
+		}
+	});
+}
+
+function storeLostToken(email, fn) {
+	// Bytesize
+	var len = config.CRYPTO_BYTE_SIZE;
+	crypto.randomBytes(len, function(err, token) {
+		if (err) return fn(err);
+		token = token.toString('hex');
+		dynamodb.updateItem({
+				TableName: config.DDB_USER_TABLE,
+				Key: {
+					email: {
+						S: email
+					}
+				},
+				AttributeUpdates: {
+					lostToken: {
+						Action: 'PUT',
+						Value: {
+							S: token
+						}
+					}
+				}
+			},
+		 function(err, data) {
+			if (err) return fn(err);
+			else fn(null, token);
+		});
+	});
+}
+
+function sendLostPasswordEmail(email, token, fn) {
+	var subject = 'Password Lost for ' + config.EXTERNAL_NAME;
+	var lostLink = config.RESET_PAGE + '?email=' + encodeURIComponent(email) + '&lost=' + token;
+
+	ses.sendEmail({
+		Source: config.EMAIL_SOURCE,
+		Destination: {
+			ToAddresses: [
+				email
+			]
+		},
+		Message: {
+			Subject: {
+				Data: subject
+			},
+			Body: {
+				Html: {
+					Data: '<html><head>'
+					+ '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
+					+ '<title>' + subject + '</title>'
+					+ '</head><body>'
+					+ 'Please <a href="' + lostLink + '">click here to reset your password</a> or copy & paste the following link in a browser:'
+					+ '<br><br>'
+					+ '<a href="' + lostLink + '">' + lostLink + '</a>'
+					+ '</body></html>'
+				}
+			}
+		}
+	}, fn);
+}
 
 function storeUser(email, name, password, salt, fn) {
 	// Bytesize
@@ -323,7 +402,7 @@ exports.handler = function(event, context) {
 		var email = event.email;
 		var oldPassword = event.oldPassword;
 		var newPassword = event.newPassword;
-		getUser(email, function(err, correctHash, salt, verified, userId) {
+		getUserLogin(email, function(err, correctHash, salt, verified, userId) {
 			if (err) {
 				context.fail('Error in getUser: ' + err);
 			} else {
@@ -369,6 +448,37 @@ exports.handler = function(event, context) {
 				}
 			}
 		});
+	} else if(type = 'LostPassword') {
+		var email = event.email;
+		getUserLostPassword(email, function(err, emailFound) {
+			if (err) {
+				context.fail('Error in getUserFromEmail: ' + err);
+			} else if (!emailFound) {
+				console.log('User not found: ' + email);
+				context.succeed({
+					sent: false
+				});
+			} else {
+				storeLostToken(email, function(err, token) {
+					if (err) {
+						context.fail('Error in storeLostToken: ' + err);
+					} else {
+						sendLostPasswordEmail(email, token, function(err, data) {
+							if (err) {
+								context.fail('Error in sendLostPasswordEmail: ' + err);
+							} else {
+								console.log('User found: ' + email);
+								context.succeed({
+									sent: true
+								});
+							}
+						});
+					}
+				});
+			}
+		});
+	} else if(type = 'RecoverPassword') {
+		
 	} else {
 								context.fail('Method not available');
 	}
